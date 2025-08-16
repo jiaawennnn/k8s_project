eb979897c1650aabc2af0df36731ca9477dc04a2
jiaawennnn <jiawen27.chua@gmail.com>
2025-08-16 21:09:30 +0800

    Created run.sh to automate kubectl apply commands

---
+++ run.sh
@@ -0,0 +1,61 @@
+# stops if any command fails
+#!/bin/bash
+
+# This script sets up a Kubernetes environment with Minikube and deploys various services.
+# It applies Persistent Volumes, ConfigMaps, and deployments for Postgres, UI, Preprocessing, Training, and Inference services.
+set -e 
+
+echo "Starting minikube..."
+minikube start
+echo " "
+
+echo "Applying Persistent Volumes and Claims..."
+kubectl apply -f db/pv.yaml
+kubectl apply -f db/pvc.yaml
+kubectl apply -f model-pv-pvc.yaml
+echo " "
+
+echo "Applying ConfigMaps..."
+kubectl apply -f db/configmap.yaml
+echo " "
+
+echo "Deploying Postgres service..."
+kubectl apply -f db/deployment.yaml
+kubectl apply -f db/service.yaml
+echo " "
+
+echo "Deploying UI service..."
+kubectl apply -f ui/deployment.yaml
+kubectl apply -f ui/service-ui.yaml
+echo " "
+
+echo "Deploying Preprocessing service..."
+kubectl apply -f preprocessing/deployment.yaml
+kubectl apply -f preprocessing/service.yaml 
+echo " "
+
+echo "Deploying Training service..."
+kubectl apply -f training/deployment.yaml
+kubectl apply -f training/service.yaml 
+echo " "
+
+echo "Deploying Inference service..."
+kubectl apply -f inference/deployment.yaml
+kubectl apply -f inference/service.yaml 
+kubectl apply -f inference/hpa.yaml
+echo " "
+
+echo "Deploying Kubernetes Dashboard..."
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+kubectl apply -f dashboard/dashboard-adminuser.yaml -n kubernetes-dashboard
+kubectl apply -f dashboard/dashboard-clusterrole.yaml -n kubernetes-dashboard
+echo " "
+
+echo "All deployments have been applied!"
+echo "Please wait a few moments for pods to start, then run: "
+echo " "
+echo "↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓"
+echo "                   kubectl get pods                   "
+echo "↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑"
+echo " "
+echo "to check status of pods."
